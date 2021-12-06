52e83e3e594a318112ce1eadbc83f06b047ed9b6
Marcelo Pereira <marcelo96jr@gmail.com>
2021-12-06 17:41:44 -0300

    chore: add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
+import Head from 'next/head';
+
 import styles from '../styles/styles.module.scss';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>
-        Primeiro projeto com <span>NextJS</span>
-      </h1>
-    </div>
+    <>
+      <Head>
+        <title>Board | Organizando tarefas</title>
+      </Head>
+      <div>
+        <h1 className={styles.title}>
+          Primeiro projeto com <span>NextJS</span>
+        </h1>
+      </div>
+    </>
   );
 }
