ecb5dfe5761dda6cdbc4c5a0ec899b3ab8cb4bb8
Marcelo Pereira <marcelo96jr@gmail.com>
2021-12-06 15:36:39 -0300

    chore: starting project with typescript and sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/styles.module.scss';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={styles.title}>
+        Primeiro projeto com <span>NextJS</span>
+      </h1>
+    </div>
+  );
+}
