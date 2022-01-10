ce16fbc4c3ba46e00546d2b4d130be9c053951c9
Marcelo Pereira <marcelo96jr@gmail.com>
2022-01-10 12:00:36 -0300

    chore: add donate page

---
+++ src/pages/donate/index.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable @next/next/no-img-element */
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/client';
+import Head from 'next/head';
+
+import styles from './styles.module.scss';
+
+interface DonateProps {
+  user: {
+    nome: string;
+    id: string;
+    image: string;
+  };
+}
+
+export default function Donate({ user }: DonateProps) {
+  return (
+    <>
+      <Head>
+        <title>Ajude a plataforma Board a ficar online</title>
+      </Head>
+
+      <main className={styles.container}>
+        <img src="/images/rocket.svg" alt="Seja apoiador" />
+
+        <div className={styles.vip}>
+          <img src={user.image} alt="Foto de perfil do usuário" />
+          <span>Parabéns, você é um novo apoiador!</span>
+        </div>
+
+        <h1>Seja um apoiador desse projeto 🏆</h1>
+        <h3>
+          Contribua com apenas <span>R$ 1,00</span>
+        </h3>
+        <strong>Apareça em nossa home, tenha funcionalidades exclusivas</strong>
+      </main>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if (!session?.id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  const user = {
+    nome: session?.user.name,
+    id: session?.id,
+    image: session?.user.image,
+  };
+
+  return {
+    props: { user },
+  };
+};
